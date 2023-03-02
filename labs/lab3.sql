b092b3453ddc66f5d07299dac6f2cac06bbd1688
tzou2024 <tzou@olin.edu>
2023-03-02 13:38:38 +0000

    change = to in in nested queries lab3

--- labs/lab3.sql
+++ labs/lab3.sql
@@ -8,28 +8,28 @@ SELECT * FROM VIEWING;
 
 -- 1. Find the names and addresses of all renters who have viewed properties.
 
-SELECT Fname, LName, Address FROM RENTER WHERE RenterNo = 
+SELECT Fname, LName, Address FROM RENTER WHERE RenterNo IN 
     ( SELECT RenterNo FROM VIEWING );
 
 -- 2. Find the type and number of rooms of each property handled by staff members who are managers.
 
-SELECT Rooms, Type FROM PROPERTY WHERE StaffNo = 
+SELECT Rooms, Type FROM PROPERTY WHERE StaffNo IN 
 (SELECT StaffNo FROM STAFF WHERE Job = 'manager');
 
 -- 3. Find the full name of each person owning an apartment.
-SELECT FName, LName from OWNER WHERE OwnerNo =
+SELECT FName, LName from OWNER WHERE OwnerNo IN
 (SELECT OwnerNo FROM PROPERTY WHERE Type = 'apartment');
 
 -- 4. Find the name and branch number of each staff member handling properties in Cork.
-SELECT FName, LName, BranchNo FROM STAFF WHERE StaffNo = 
+SELECT FName, LName, BranchNo FROM STAFF WHERE StaffNo IN 
 (SELECT StaffNo FROM PROPERTY WHERE City = 'Cork');
 
 -- 5. Find the names of renters who have viewed properties with at least 3 rooms.
-SELECT Fname, Lname FROM RENTER where RenterNo = 
-(SELECT RenterNo FROM VIEWING WHERE PropertyNo = (SELECT PropertyNo FROM PROPERTY WHERE Rooms > 2));
+SELECT Fname, Lname FROM RENTER where RenterNo IN 
+(SELECT RenterNo FROM VIEWING WHERE PropertyNo IN (SELECT PropertyNo FROM PROPERTY WHERE Rooms > 2));
 
 -- 6. Find the names of staff members who handle properties owned by Woody Allen.
 -- SELECT FName, LName from STAFF WHERE STAFFNO = 
-SELECT Fname, LName FROM STAFF WHERE STAFFNo = 
+SELECT Fname, LName FROM STAFF WHERE STAFFNo IN 
 (SELECT StaffNo FROM PROPERTY WHERE OwnerNo = 
 (SELECT OwnerNo FROM OWNER WHERE FName = 'Woody' and LName = 'Allen'));
